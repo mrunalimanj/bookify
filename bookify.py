b21ce538b644ae6866d03896278cce1c99a5c029
Vainavi Viswanath <vainaviv2001@berkeley.edu>
2021-10-23 14:09:24 -0700

    get emotions done

---
+++ bookify.py
@@ -0,0 +1,16 @@
+import text2emotion as te
+from collections import Counter
+
+text = "Young and quirky Louisa Lou Clark (Emilia Clarke) moves from one job to the next to help her family make ends meet. Her cheerful attitude is put to the test when she becomes a caregiver for Will Traynor (Sam Claflin), a wealthy young banker left paralyzed from an accident two years earlier. Will's cynical outlook starts to change when Louisa shows him that life is worth living. As their bond deepens, their lives and hearts change in ways neither one could have imagined. But Will doesn't change his mind. On the night of Will's flight to Switzerland, Louisa visits him one last time. They agree that the past six months have been the best in their lives. He dies shortly after in the clinic, and it is revealed that he left Louisa a considerable inheritance, meant to continue her education and to fully experience life. The novel ends with Louisa at a cafÃ© in Paris, reading Will's last words to her in a letter, that tell her to 'live well'."
+emotions = te.get_emotion(text)
+k = Counter(emotions)
+high = k.most_common(3)
+print(high)
+
+query = ""
+for e in high:
+    print(e[0])
+    query += (e[0])
+    query += " "
+query += "songs"
+print(query)
